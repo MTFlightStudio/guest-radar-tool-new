8c8d72bbc5f359694d05390efef88c8fec7f0f4a
Matt  Toland <matt.toland@FS079.local>
2024-10-22 18:27:32 +0100

    Update with latest changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.js
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        'default': '0 1px 3px rgba(0, 0, 0, 0.7)',
+      },
+    },
   },
-  plugins: [],
-};
-
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.shadow-text': {
+          textShadow: '0 1px 3px rgba(0, 0, 0, 0.7)',
+        },
+      }
+      addUtilities(newUtilities)
+    }
+  ]
+}
